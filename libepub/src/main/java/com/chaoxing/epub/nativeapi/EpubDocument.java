f2eb73314cd507562d5ce1a0674c8365ec41472f
HUWEI <huwei2048@gmail.com>
2018-04-24 13:57:13 +0800

    night

--- libepub/src/main/java/com/chaoxing/epub/nativeapi/EpubDocument.java
+++ libepub/src/main/java/com/chaoxing/epub/nativeapi/EpubDocument.java
@@ -6,8 +6,17 @@ public class EpubDocument {
         System.loadLibrary("epub");
     }
 
+    public static final String META_FORMAT = "format";
+    public static final String META_INFO_AUTHOR = "info:Author";
+    public static final String META_INFO_TITLE = "info:Title";
+
     public static native String sayHello();
 
     public static native EpubDocument openDocument(String path);
+
+    public static native String getMetaData(String key);
+
+    public static native Chapter[] loadChapter();
+
 
 }
---
+++ libepub/src/main/java/com/chaoxing/epub/nativeapi/Page.java
@@ -0,0 +1,4 @@
+package com.chaoxing.epub.nativeapi;
+
+public class Page {
+}
